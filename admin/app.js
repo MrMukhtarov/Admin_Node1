e8226d39225f24cde1285c56a9dd4454cfc427d3
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2022-10-07 19:06:23 +0400

    admin

---
+++ admin/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const connection = require('./database/db')
+connection.once('open', () => console.log('Connection MongoDB'))
+connection.on('error', () => console.log(error))
+
+//AdminBro
+const AdminBro = require('admin-bro')
+const expressAdminBro = require('@admin-bro/express')
+const mongooseAdminBro = require('@admin-bro/mongoose')
+
+//Model
+const User = require('./models/User')
+const Post = require('./models/Post')
+
+AdminBro.registerAdapter(mongooseAdminBro)
+const AdminBroOptions = {resources: [User,Post]}
+
+const adminBro = new AdminBro(AdminBroOptions)
+const router = expressAdminBro.buildRouter(adminBro)
+app.use(adminBro.options.rootPath, router)
+
+app.get('/', (req,res) => {
+    res.send('dashboard connect')
+})
+
+app.listen(3001, () => {
+    console.log('server up http://localhost:3002/admin');
+})
+
+// getusers(){
+users=_yfg8syrgnbfyengesby
+//}
